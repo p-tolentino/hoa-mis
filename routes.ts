10f1fcf6833bbe64788fd1037252453960e11861
p-tolentino <95534051+p-tolentino@users.noreply.github.com>
2024-01-15 21:09:16 +0800

    added login-register with Google OAuth

---
+++ routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are used for authentication
+ * Logged-in users will be redirected to /settings or /dashboard
+ * @type {string[]}
+ */
+export const authRoutes = ["/login", "/register"];
+
+/**
+ * The prefix for API Authentication routes
+ * Rotues that start with this prefix are used for API Authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * Default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
