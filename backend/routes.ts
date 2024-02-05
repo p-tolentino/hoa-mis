12a7b7995b48baa69533cdbd884edda6129dabde
p-tolentino <95534051+p-tolentino@users.noreply.github.com>
2024-02-06 00:42:23 +0800

    redirect first-time

--- backend/routes.ts
+++ backend/routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/', '/new-verification']
+export const publicRoutes = ["/", "/new-verification"];
 
 /**
  * An array of routes that are used for authentication
@@ -11,25 +11,26 @@ export const publicRoutes = ['/', '/new-verification']
  * @type {string[]}
  */
 export const authRoutes = [
-  '/login',
-  '/register'
+  "/login",
+  "/register",
   //   '/admin/membership',
   //   '/admin/membership/user-management',
   //   '/admin/membership/association-contact-directory',
   //   '/admin/membership/property-management',
   //   '/admin/membership/membership-form'
-]
+];
 
 /**
  * The prefix for API Authentication routes
  * Routes that start with this prefix are used for API Authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = '/api/auth'
+export const apiAuthPrefix = "/api/auth";
 
 /**
  * Default redirect path after logging in
  * @type {string}
  */
+export const DEFAULT_LOGIN_REDIRECT = "/user/setup"; // TODO: Change to user dashboard
 
-export const DEFAULT_LOGIN_REDIRECT = '/user/setup'
+export const DEFAULT_FIRST_LOGIN_REDIRECT = "/admin/membership/membership-form";
